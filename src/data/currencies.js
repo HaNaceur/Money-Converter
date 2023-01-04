d84199b3b18c84001d31de4b13acec7c9352497f
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2023-01-04 16:27:57 +0100

    feat: dynamisation of the selection by currencies &creating id

--- src/data/currencies.js
+++ src/data/currencies.js
@@ -1,130 +1,167 @@
 export default [
   {
+    id: 0,
     name: 'Australian Dollar',
     rate: 1.665247,
   },
   {
+    id: 1,
     name: 'Bulgarian Lev',
     rate: 1.955843,
   },
   {
+    id: 2,
     name: 'Brazilian Real',
     rate: 4.877723,
   },
   {
+    id: 3,
     name: 'Canadian Dollar',
     rate: 1.459549,
   },
   {
+    id: 4,
     name: 'Swiss Franc',
     rate: 1.063547,
   },
   {
+    id: 5,
     name: 'Chinese Renminbi Yuan',
     rate: 7.663895,
   },
   {
+    id: 6,
     name: 'Czech Koruna',
     rate: 25.303513,
   },
   {
+    id: 7,
     name: 'Danish Krone',
     rate: 7.471905,
   },
   {
+    id: 8,
     name: 'British Pound',
     rate: 0.848099,
   },
   {
+    id: 9,
     name: 'Hong Kong Dollar',
     rate: 8.527761,
   },
   {
+    id: 10,
     name: 'Croatian Kuna',
     rate: 7.461611,
   },
   {
+    id: 11,
     name: 'Hungarian Forint',
     rate: 339.010251,
   },
   {
+    id: 12,
     name: 'Indonesian Rupiah',
     rate: 15244.461735,
   },
   {
+    id: 13,
     name: 'Israeli New Sheqel',
     rate: 3.759468,
   },
   {
+    id: 14,
     name: 'Indian Rupee',
     rate: 78.266835,
   },
   {
+    id: 15,
     name: 'Icelandic Króna',
     rate: 139.283067,
   },
   {
+    id: 16,
     name: 'Japanese Yen',
     rate: 120.425258,
   },
   {
+    id: 17,
     name: 'South Korean Won',
     rate: 1323.751325,
   },
   {
+    id: 18,
     name: 'Mexican Peso',
     rate: 21.229867,
   },
   {
+    id: 19,
     name: 'Malaysian Ringgit',
     rate: 4.604002,
   },
   {
+    id: 20,
     name: 'Norwegian Krone',
     rate: 10.271817,
   },
   {
+    id: 21,
     name: 'New Zealand Dollar',
     rate: 1.733181,
   },
   {
+    id: 22,
     name: 'Philippine Peso',
     rate: 55.663969,
   },
   {
+    id: 23,
     name: 'Polish Złoty',
     rate: 4.310683,
   },
   {
+    id: 24,
     name: 'Romanian Leu',
     rate: 4.808434,
   },
   {
+    id: 25,
     name: 'Russian Ruble',
     rate: 71.643333,
   },
   {
+    id: 26,
     name: 'Swedish Krona',
     rate: 10.592243,
   },
   {
+    id: 27,
     name: 'Singapore Dollar',
     rate: 1.524971,
   },
   {
+    id: 28,
     name: 'Thai Baht',
     rate: 34.637741,
   },
   {
+    id: 29,
     name: 'Turkish Lira',
     rate: 6.75515,
   },
   {
+    id: 30,
     name: 'United States Dollar',
     rate: 1.094103,
   },
   {
+    id: 31,
     name: 'South African Rand',
     rate: 16.779419,
+  },
+  {
+    id: 32,
+    name: 'Bitcoin',
+    rate: 16315.12,
   },
 ];
--- src/tools/index.js
+++ src/tools/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
 export function roundAtDecimal(number, decimal) {
-    const pow = 10 ** decimal;
-    return Math.round(number * pow) / pow;
-  }
-  
+  const pow = 10 ** decimal;
+  return Math.round(number * pow) / pow;
+}
